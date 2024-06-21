7d48a490e68ec1911db71b4cc6e9c98943353868
Christian Aromin <caromin19@gmail.com>
2024-06-21 15:01:59 -0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Data } from './models/data';
 
 @Component({
   selector: 'app-root',
@@ -18,43 +17,15 @@ export class AppComponent {
   private startDate: any= null;
   private llpOrAfs: any= null;
   private roleClearance: any= null;
+  private roleDTE = 'AFS';
 
   public filteredResult: object[] = [];
-
-
-  // Example 
-  //   {
-  //     "score": 0.0,
-  //     "resultItem": {
-  //         "class": "paris.web.app.RequisitionMeta",
-  //         "id": 525585,
-  //         "number": "5126632",
-  //         "title": "Governance Executive Lead",
-  //         "projectName": "Assessment",
-  //         "client": "BATH & BODY WORKS INC",
-  //         "status": "Open - Need Project Feedback",
-  //         "locationPath": "Midwest > USA > Midwest > Columbus",
-  //         "locationRadius": "In Country",
-  //         "workLocation": "Columbus",
-  //         "primaryContact": "Schenk,Luke",
-  //         "billable": "Chargeable - Unsold",
-  //         "owningOrg": "LLP",
-  //         "fromLevel": 4,
-  //         "toLevel": 4,
-  //         "startDate": "2025-02-03",
-  //         "createDate": "2024-01-22",
-  //         "roleDTE": "LLP",
-  //         "roleClearance": "Clearance Not Applicable",
-  //         "locationType": "Remote",
-  //         "acceptingResumes": "N",
-  //         "percentOnsite": 0
-  //     }
-  // }
 
   constructor(private http: HttpClient) { }
 
   generateCsv() {
     this.http.get<any>(this.jsonResults).subscribe(json => {
+      console.log(json);
       const data: any[] = json.data;
       const targetLevels = [this.roleLevel + 1, this.roleLevel - 1];
 
@@ -65,7 +36,9 @@ export class AppComponent {
               if (this.startDate == null || element.resultItem.startDate?.includes(this.startDate)) {
                 if (this.llpOrAfs == null || element.resultItem.roleDTE?.includes(this.llpOrAfs)) {
                   if (this.roleClearance == null || element.resultItem.roleClearance?.includes(this.roleClearance)) {
-                    this.filteredResult.push(element.resultItem);
+                    if (element.resultItem.roleDTE?.includes(this.roleDTE)) {
+                      this.filteredResult.push(element.resultItem);
+                    }
                   }
                 }
               }
@@ -78,7 +51,7 @@ export class AppComponent {
   }
 
   export() {
-    const headerList = ['number', 'title', 'fromLevel', 'toLevel', 'status', 'billable', 'startDate', 'acceptingResumes', 'locationType', 'projectName', 'roleClearance'];
+    const headerList = ['number', 'title', 'createDate', 'fromLevel', 'toLevel', 'status', 'billable', 'startDate', 'acceptingResumes', 'locationType', 'projectName', 'roleClearance'];
     const csvData = this.convertToCSV(this.filteredResult, headerList);
     this.downloadCSV(csvData, 'results.csv');
   }
